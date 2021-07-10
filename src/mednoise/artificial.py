8e5270b36782543f4d68698475b94cd398397789
Ravi Bandaru <ravi14.bandaru@gmail.com>
2021-07-10 12:06:06 -0400

    Update artificial.py

--- src/mednoise/artificial.py
+++ src/mednoise/artificial.py
@@ -1,1 +1,11 @@
-
+def artificial_complete():
+  print("This feature is still being worked on.")
+  
+def artificial_calculator():
+  print("This feature is still being worked on.")
+  
+def artificial_analyzer():
+  print("This feature is still being worked on.")
+  
+def artificial_isolator():
+  print("This feature is still being worked on.")
