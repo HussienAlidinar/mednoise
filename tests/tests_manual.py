f874a3e3add0a454232402192265ef49f0967ed5
Ravi Bandaru <ravi14.bandaru@gmail.com>
2021-07-16 21:15:30 -0400

    Update tests_manual.py

--- tests/tests_manual.py
+++ tests/tests_manual.py
@@ -1,6 +1,2 @@
 from mednoise import manual as md
 md.manual_merge("tests/images/*", (0,0,0), (255, 0, 0))
-restraints = [(473,91),(214,601),(764,626)]
-md.manual_edit("tests/images/*", xy = restraints)
-md.manual_primer("tests/images/*")
-md.manual_find("tests/images/*")
